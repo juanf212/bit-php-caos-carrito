742648c5d090b4730ddbbe25abd6d44e10c7cd88
Juan Forero <juanf212@gmail.com>
2018-06-15 14:42:57 -0500

    Barra de navegacion y categorias

--- index.php
+++ index.php
@@ -1,1 +1,47 @@
 <?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Efecto Caos</title>
+    <link rel="stylesheet" href="bootstrap.css">
+</head>
+<body>
+    <!-- 1. nav bar -->
+    <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">CAOS</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Sobre Caos</a>
+            <a class="nav-item nav-link" href="#">Contacto</a>
+            <a class="nav-item nav-link disabled" href="#">Carrito de compras</a>
+            </div>
+        </div>
+    </nav>
+    </div>
+    <!-- 2. categorias productos -->
+    <div>
+    <div class="row">
+            <!-- 1.1. Columna boton Home -->
+            <div class="col"><h5>Bufandas</h5></div>
+            <!-- 1.2. Columna boton Home -->
+            <div class="col"><h5>Turbantes</h5></div>
+            <!-- 1.3. Columna boton Home -->
+            <div class="col"><h5>Accesorios</h5></div>
+            <!-- 1.4. Columna boton Home -->
+            <div class="col-md-auto"><h5>Arte</h5></div>
+    </div>
+    </div>
+    <!-- 3. cards productos -->
+    <div></div>
+</body>
+</html>
