611a63549658ce72dd01f4a0269c08627c914cfd
Juan Forero <juanf212@gmail.com>
2018-08-13 10:53:23 -0500

    [refactor] sesion - cambio consulta (SELECT)

--- carrito.php
+++ carrito.php
@@ -1,8 +1,11 @@
 <?php 
+    
     require('conexion.php');
+    require('sesion.php');
 
+    
     $productosCarrito = consulta("SELECT *, productos.nombre as nombre_producto, precios.nombre as nombre_precio FROM carrito
     JOIN productos ON productos.id=carrito.producto_id
-    JOIN precios ON precios.id=carrito.precio_id");
+    JOIN precios ON precios.id=carrito.precio_id WHERE carrito.cliente='$clienteId' ");
 
     require('vistas/carrito.php');
