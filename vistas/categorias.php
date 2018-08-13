b0ee8da0fc7a16ded231a6d13123a30935871663
Juan Forero <juanf212@gmail.com>
2018-08-13 10:59:29 -0500

    [refactor] creacion archivo categorias.php

---
+++ vistas/categorias.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="row">
+            
+            <div class="col"><h5 class="mt-3">Bufandas</h5></div>
+            
+            <div class="col"><h5 class="mt-3">Turbantes</h5></div>
+            
+            <div class="col"><h5 class="mt-3">Accesorios</h5></div>
+            
+            <div class="col-md-auto"><h5 class="mt-3">Arte</h5></div>
+    </div>
+</body>
+</html>
