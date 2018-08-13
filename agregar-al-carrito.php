6e913ce9041ce08add2c4b551ba19cf05e95c5de
Juan Forero <juanf212@gmail.com>
2018-08-13 10:51:45 -0500

    [fix] sesion - cambio en consulta (INSERT) y [refactor] uso sesion.php

--- agregar-al-carrito.php
+++ agregar-al-carrito.php
@@ -1,11 +1,14 @@
 <?php
     require('conexion.php');
     require('utilities.php');
+    require('sesion.php');
+
+    $clienteId = $_SESSION['cliente'];
 
     $medidaId = $_GET['medida'];
     $productoId = $_GET['producto'];
 
 
-    consulta("INSERT INTO carrito (producto_id, precio_id) VALUES ('$productoId', '$medidaId')");
+    consulta("INSERT INTO carrito (producto_id, precio_id, cliente) VALUES ('$productoId', '$medidaId', '$clienteId')");
 
     redireccionar('carrito.php');
